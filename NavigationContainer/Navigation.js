463577d4653cacf01b465f46cdceeb1554587897
imene78 <95373809+imene78@users.noreply.github.com>
2022-01-31 16:08:41 +0100

    envoie test

---
+++ NavigationContainer/Navigation.js
@@ -0,0 +1,30 @@
+// In App.js in a new project
+
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Presentation from '../pages/Presentation';
+import Inscription from '../pages/Inscription';
+import Connexion from '../pages/Connexion';
+
+
+const Stack = createNativeStackNavigator();
+
+function Navigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Inscription">
+   
+      {/*<Stack.Screen name="Presentation" component={Presentation} />*/}
+
+      <Stack.Screen name="Inscription" component={Inscription} />
+      <Stack.Screen name="Connexion" component={Connexion} />
+      </Stack.Navigator>
+      
+    </NavigationContainer>
+  );
+}
+
+export default Navigation;
