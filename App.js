0d05b1a5146dce67792df49f3135acf9bd3188a7
DONGVK <jean.dong2000@gmail.com>
2022-01-31 16:21:29 +0100

    navigation

--- App.js
+++ App.js
@@ -1,28 +1,35 @@
-import { StyleSheet, Text, View } from 'react-native';
-import { StatusBar } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from "react-native";
+//==========
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+import Navigation from "./NavigationContainer/Navigation";
+import Presentation from "./pages/Presentation";
+import Inscription from "./pages/Inscription";
+import Connexion from "./pages/Connexion";
+import Menu from "./pages/Menu";
 
-import Navigation from './NavigationContainer/Navigation';
-import Presentation from './pages/Presentation';
-import Inscription from './pages/Inscription';
-import Connexion from './pages/Connexion';
-import Menu from './pages/Menu';
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle = "light-content"/>
-      <Navigation/>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer style={styles.container}>
+      <Stack.Navigator initialRouteName="Inscription">
+        <Stack.Screen name="Presentation" component={Presentation} />
+        <Stack.Screen name="Inscription" component={Inscription} />
+        <Stack.Screen name="Connexion" component={Connexion} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
